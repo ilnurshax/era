740934f6e897d13d746bcdda70171470fac3c431
nur.shax <ilnur888@gmail.com>
2019-08-16 09:10:48 +0300

    Add the DbSpecification abstract class to more easiest define the Specifications which can be used in repository queries

---
+++ src/Specifications/DbSpecification.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nur
+ * Date: 16.08.2019
+ * Time: 9:09
+ */
+
+namespace Ilnurshax\Era\Specifications;
+
+
+abstract class DbSpecification implements Specification, SpecificationAsQuery
+{
+
+}
